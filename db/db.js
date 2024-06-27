55d41297a10ddcac577755430d16fdd46171e618
MarcosSosaBackRock <132264637+MarcosSosaBackRock@users.noreply.github.com>
2024-06-26 21:19:07 -0300

    updatedVercelPRoject

--- db/db.js
+++ db/db.js
@@ -1,11 +1,19 @@
+require('dotenv').config();
+
 const mySql = require('mysql2');
 
 const connection = mySql.createConnection(
     {
+        /*
         host : 'localhost',
         user: 'root',
         password : 'Loveisall77',
         database: 'usuarios_db'
+        */
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     });
 
 
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 /*--------SERVIDOR ESTATICO CON EXPRESS-------*/
 let port = 3000;
 const express = require('express');
